7a2ddd70be0af9b0fdf90d987251d5a5c85c75b8
tensept <vankaew58@gmail.com>
2024-02-20 19:30:36 +0700

    lab17

--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatController.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+
 @Controller
 public class ChatController {
     @MessageMapping("/chat.sendMessage")
--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
@@ -4,6 +4,7 @@ package com.websocket.demo.chat;
 import lombok.Builder;
 import lombok.Getter;
 
+
 @Getter
 @Builder
 public class ChatMessage {
